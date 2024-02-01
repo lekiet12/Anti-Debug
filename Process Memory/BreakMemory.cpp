77913a6d089a0c17d77abc1ef4f499ae5a86f7fd
Kaito <153298724+lekiet12@users.noreply.github.com>
2024-02-01 17:24:40 +0700

    Update BreakMemory.cpp

--- Process Memory/BreakMemory.cpp
+++ Process Memory/BreakMemory.cpp
@@ -4,13 +4,15 @@ using namespace std;
 bool IsDebugged()
     
 {
-    CONTEXT ctx;
-    ZeroMemory(&ctx, sizeof(CONTEXT)); 
-    ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS; 
+    CONTEXT ctx;// khai báo ctx để lưu thông tin của các luồng thực thi 
+    ZeroMemory(&ctx, sizeof(CONTEXT)); // Thiết lập các byte trong trong ctx về 0
+    ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS; // Chỉ định chỉ lấy thông tin của các thanh ghi gỡ lỗi.
 
+    // Lấy thông tin của luồng thực thi hiện tại và lưu vào biến ctx
     if(!GetThreadContext(GetCurrentThread(), &ctx))
         return false;
 
+    // Nếu bất kỳ thanh ghi gỡ lỗi nào khác 0 thì return true -> có debug
     return ctx.Dr0 || ctx.Dr1 || ctx.Dr2 || ctx.Dr3;
 }
 int main(){
