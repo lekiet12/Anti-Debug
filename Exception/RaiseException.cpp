3facd0107ea3a42f7dd5e6487ef2f5977ddc31c1
Kaito <153298724+lekiet12@users.noreply.github.com>
2024-02-01 16:46:08 +0700

    Update RaiseException.cpp

--- Exception/RaiseException.cpp
+++ Exception/RaiseException.cpp
@@ -8,10 +8,11 @@ bool Check()
     {
         // Tạo một ngoại lệ DBG_CONTROL_C, DBG_CONTROL_C tương tự như breakpoint. 
         // RaiseException(DBG_CONTROL_C, 0, 0, NULL) chỉ tạo thành công trong môi trường debug.
-        // Nếu tạo thành công thì sẽ return true, ngược lại sẽ thực thi lệnh trong khối except
         RaiseException(DBG_CONTROL_C, 0, 0, NULL);
         return true;
     }
+    // GetExceptionCode() == DBG_CONTROL_C Kiểm tra ngoại lệ có phải là DBG_CONTROL_C không.
+    //Nếu phải thì sẽ thực thi khối lệnh trong except. Nếu không thì sẽ tiếp tục tìm kiếm 
     __except (GetExceptionCode() == DBG_CONTROL_C ? EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH)
     {
         return false;
